96607aac2e77ec7d841ab160756300c395231030
yushsato <yushsato@student.42tokyo.jp>
2023-11-13 20:01:14 +0900

    update: parse and order

--- main.c
+++ main.c
@@ -6,15 +6,58 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 18:58:30 by yushsato          #+#    #+#             */
-/*   Updated: 2023/11/13 15:59:37 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/11/13 20:00:24 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-__attribute__((destructor))	static void	destructor(void)
+// __attribute__((destructor))	static void	destructor(void)
+// {
+// 	system("leaks -q push_swap");
+// }
+
+static int	is_num(char *num)
 {
-	system("leaks -q push_swap");
+	if (*num == 48 && *(num + 1) == '\0')
+		return (1);
+	if (*num == '-')
+		num++;
+	if (49 <= *num && *num <= 59)
+	{
+		while (48 <= *num && *num <= 59)
+			num++;
+		if (*num == '\0')
+			return (1);
+	}
+	return (0);
+}
+
+static int	is_parsable(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc >= 2)
+	{
+		argv++;
+		i = 0;
+		while (i < argc - 1)
+		{
+			if (!is_num(argv[i]))
+				return (0);
+			j = 0;
+			while (j < argc - 1)
+			{
+				if (!ft_memcmp(argv[i], argv[j], ft_strlen(argv[i]) + 1)
+					&& i != j)
+					return (0);
+				j++;
+			}
+			i++;
+		}
+	}
+	return (1);
 }
 
 static char	**parser(int argc, char **argv)
@@ -32,17 +75,6 @@ static char	**parser(int argc, char **argv)
 			*ptr++ = ft_strdup(*(++argv));
 	}
 	return (values);
-}
-
-static int	is_include(int *ary, int *i)
-{
-	while (*ary != 0)
-	{
-		if (*ary == *i)
-			return (1);
-		ary++;
-	}
-	return (0);
 }
 
 static int	*order(char **c)
@@ -64,8 +96,6 @@ static int	*order(char **c)
 		while (c[j++] != NULL)
 			if (ft_atoi(c[j - 1]) < ft_atoi(c[i - 1]))
 				n++;
-		while (is_include(ret, &n))
-			n++;
 		ret[i - 1] = n;
 	}
 	return (ret);
@@ -77,11 +107,15 @@ int	main(int argc, char **argv)
 	int		*ordered;
 	int		i;
 
+	if (argc == 1)
+		return (0);
+	if (!is_parsable(argc, argv))
+	{
+		ft_printf("Error\n");
+		return (1);
+	}
 	parsed = parser(argc, argv);
 	ordered = order(parsed);
-	i = 0;
-	while (ordered[i])
-		printf("%d, ", ordered[i++]);
 	i = 0;
 	while (parsed[i] != NULL)
 		free(parsed[i++]);
