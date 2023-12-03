85866a28ecba5cc081c55521e19d5ce6ef27f77c
yukun <yukun0507@outlook.jp>
2023-12-03 18:44:19 +0900

    update

--- ps_lib/ps_argo_all.c
+++ ps_lib/ps_argo_all.c
@@ -26,7 +26,7 @@ static void	a_to_b(t_stack *stack)
 		while (stack->a != NULL && ps_node_len(stack->a) > 0 && i < s * 2)
 		{
 			if ((max <= stack->a->num && stack->a->num < max + s)
-				|| max + s <= stack->a->num && stack->a->num < max + s * 2)
+				|| (max + s <= stack->a->num && stack->a->num < max + s * 2))
 				i++;
 			if (max <= stack->a->num && stack->a->num < max + s)
 				ps_stack_op("pb", stack);
