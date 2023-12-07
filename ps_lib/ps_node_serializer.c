8e8ee0e99c09e1ef7b675bcb3b196433b0fd757b
yukun <yukun0507@outlook.jp>
2023-12-07 20:54:08 +0900

    update

--- ps_lib/ps_node_serializer.c
+++ ps_lib/ps_node_serializer.c
@@ -6,22 +6,27 @@
 /*   By: yushsato <yushsato@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 18:29:15 by yushsato          #+#    #+#             */
-/*   Updated: 2023/11/24 18:29:16 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/12/07 20:53:26 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+#include "limits.h"
+
+static int	exitforce(int i)
+{
+	exit(i);
+	return (1);
+}
 
 t_node	*ps_node_serializer(char **val)
 {
-	t_node	*ret;
-	t_node	*current;
-	t_node	*prev;
-	int		i;
-	int		j;
+	t_node			*ret;
+	t_node			*current;
+	static t_node	*prev;
+	static int		i = -1;
+	int				j;
 
-	prev = NULL;
-	i = -1;
 	while (val[++i] != NULL)
 	{
 		current = ft_calloc(1, sizeof(t_node));
@@ -34,7 +39,11 @@ t_node	*ps_node_serializer(char **val)
 		j = -1;
 		while (val[++j] != NULL)
 			if (ft_atoi(val[i]) > ft_atoi(val[j]))
+			{
+				if (current->num == INT_MAX)
+					ft_printf("Error\n", exitforce(1));
 				current->num += 1;
+			}
 	}
 	return (ret);
 }
