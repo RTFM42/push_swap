c455e64d38114f5ca546f37960df88138ce19f20
yukun <yukun0507@outlook.jp>
2023-12-07 17:21:45 +0900

    update

--- lib/ft_printf/libft/ft_calloc.c
+++ lib/ft_printf/libft/ft_calloc.c
@@ -6,13 +6,13 @@
 /*   By: yushsato <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 18:51:27 by yushsato          #+#    #+#             */
-/*   Updated: 2023/12/07 17:01:13 by yushsato         ###   ########.fr       */
+/*   Updated: 2023/12/07 17:21:12 by yushsato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
-#include "unistd.h;"
+#include "unistd.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -28,7 +28,7 @@ void	*ft_calloc(size_t count, size_t size)
 	ptr = malloc(size * count);
 	if (ptr == NULL)
 	{
-		write(1, "Error\n", 1);
+		write(1, "Error\n", 6);
 		exit(1);
 	}
 	if (ptr)
